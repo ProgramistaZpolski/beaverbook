35758d62273c634c8c5f7b2e21e71a68dea99bbc
Piotr Badełek <piotrekbadelek@gmail.com>
2021-02-11 09:30:48 +0100

    Poprawki
    Spacces -> Tabs
    Require -> Require_Once

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 $site = [];
 
-$site['config'] = require 'config.php';
+$site['config'] = require_once 'config.php';
 
 require_once 'core/database/Connection.php';
 require_once 'core/database/QueryBuilder.php';
 
 $site['database'] = new QueryBuilder(
-    Connection::make($site['config']['database'])
+	Connection::make($site['config']['database'])
 );
