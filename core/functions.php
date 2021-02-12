819334c4698921b5240b4d878ca6f155c9794ce6
Piotr Badełek <piotrbadelek@protonmail.com>
2021-02-12 18:34:50 +0100

    Ładniejsze 403

--- core/functions.php
+++ core/functions.php
@@ -5,7 +5,7 @@ class Utils
 	public static function denyAccess(String $moreInfo = "")
 	{
 		header('HTTP/1.0 403 Forbidden');
-		die("403 Access Denied. " . $moreInfo);
+		die("<p style='font-size: 1.25rem; font-family: Ubuntu, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Helvetica, Cantarell, Oxygen, Arial, Tahoma, Geneva, Verdana, 'Noto Sans', sans-serif;'>403 Access Denied. " . $moreInfo . "</p>");
 	}
 
 	public static function dump($data) {
